046fcbfbcb26311f7c5f1cfb505952e0d058418b
Julius170 <juliusyoola5@gmail.com>
2022-11-02 01:22:17 +0100

    C++ starter

--- main.cpp
+++ main.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
 
-int main() {
-    int file_size = 0, 
-    counter = 0;
-    double sales = 9.99;
-    std::cout << file_size;
-    return 0;
+
+// intitalizing Variables 
+// int main() {
+//     int file_size = 0, 
+//     counter = 0;
+//     double sales = 9.99;
+//     std::cout << file_size;
+//     return 0;
+// }
+
+
+// Assigning Variables
+// int main() {
+//     int a = 1;
+//     int b = 2;
+//     int c = 0;
+//     c = b;
+//     b = a;
+//     a = c;
+
+//     std::cout << a;
+//     std::cout << b; 
+//     return 0;
+// }
+
+
+// Constants
+// int main() {
+//     const double pi = 3.14;
+//     std::cout << pi; 
+// }
+
+
+// Mathematical Expressions
+// int main() {
+//     int x = 10;
+//     int y = 90;
+//     int z = y * x;
+//     std::cout << z; 
+// }
+
+
+// Increment & Decrement
+// int main() {
+//     int x = 10;
+//     int y = x++;  // x = 11, y = 10
+//     int z = ++x;  // x = 11, z = 11
+//     std::cout << z;
+//     return 0;
+// }
+
+// int main() {
+//     int x = 10;
+//     int y = x++;  // x = 11, y = 10
+//     int z = ++x;  // x = 11, z = 11
+//     std::cout << z;
+//     return 0;
+// }
+
+int main () {
+    int x = 0;
+    std::cout << "x = " << x;
 }
